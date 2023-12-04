13e5f2e583201a30b713e4bd22f0d3b4ec658572
hovohalstyan <galstyan_h99@mail.ru>
2023-12-04 21:27:20 +0400

    edit users routers file path name

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import {Router} from "express";
 import UsersController from "../controllers/UsersController.js";
 import validate from "../middlewares/validate.js";
 import usersSchema from "../schema/usersSchema.js";
@@ -7,24 +7,24 @@ const router = Router();
 
 
 router.post(
-  '/register',
-  validate(usersSchema.register),
-  UsersController.register
+    '/register',
+    validate(usersSchema.register),
+    UsersController.register
 );
 
 router.post('/activate', UsersController.activate)
 
 router.post('/login', validate(usersSchema.login), UsersController.login)
 
-router.get('/profile',UsersController.profile)
+router.get('/profile', UsersController.profile)
 
-router.post('/oauth',UsersController.oauth)
+router.post('/oauth', UsersController.oauth)
 
-router.post('/sendPasswordRecoveryCode',UsersController.sendPasswordRecoveryCode)
+router.post('/send-password-recovery-code', UsersController.sendPasswordRecoveryCode)
 
-router.post('/validatePasswordRecoveryCode', UsersController.validatePasswordRecoveryCode)
+router.post('/validate-password-recovery-code', UsersController.validatePasswordRecoveryCode)
 
-router.post('/passwordUpdate', validate(usersSchema.passwordUpdate), UsersController.passwordUpdate)
+router.post('/password-update', validate(usersSchema.passwordUpdate), UsersController.passwordUpdate)
 
 
 export default router;
