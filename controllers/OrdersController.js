b63b654dc76ecd4eaaa0dbf7f2d8405fc4d95f37
karenachemyan <achemyankaren@gmail.com>
2024-02-29 13:41:01 +0400

    checkout changes

--- controllers/OrdersController.js
+++ controllers/OrdersController.js
@@ -132,17 +132,17 @@ class OrdersController {
           order_id: order.id,
           user_id: userId
         },
-        //payment_method: paymentMethodToken,
-        //confirm: true 
+        payment_method: paymentMethodToken,
+        confirm: true 
       });
 
-      /*if (paymentIntent.status !== 'succeeded') {
+      if (paymentIntent.status !== 'succeeded') {
         throw HttpError(500, {
           errors: {
             error: 'Payment failed'
           }
         });
-      }*/
+      }
 
       await order.update({
         status: 'active'
